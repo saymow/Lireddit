b599f4c9dead4bd2266a81302c8475fb8081b1e1
Gustavo <gustavo.4lv5s@gmail.com>
2020-09-10 15:51:20 -0300

    chore: Apollo server graphql setup

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,35 @@
+import express from "express";
 import { MikroORM } from "@mikro-orm/core";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
 
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 
 import microConfig from "./mikro-orm.config";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  // const post = orm.em.create(Post, { title: "My first post" });
-  // await orm.em.persistAndFlush(post);
-  const posts = await orm.em.find(Post, {});
 
-  console.log(posts);
+  const app = express();
+  const PORT = process.env.PORT || "3333";
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.get("/", (_, res) => res.send("Cocô"));
+
+  app.listen(PORT, () => {
+    console.log("Server is up on:", PORT);
+  });
 };
 
 main();
