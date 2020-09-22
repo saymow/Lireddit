1e4d66ebf5c18d1e9a8c195c8a17d75df20283ce
Gustavo <gustavo.4lv5s@gmail.com>
2020-09-22 18:29:21 -0300

    chore: Next.js URQL SSR

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,10 +1,23 @@
+import { withUrqlClient } from "next-urql";
 import { NavBar } from "../components/NavBar";
+import { usePostsQuery } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
-const Index = () => (
-  <>
-    <NavBar />
-    <div>hello world</div>;
-  </>
-);
+const Index = () => {
+  const [{ data }] = usePostsQuery();
 
-export default Index;
+  return (
+    <>
+      <NavBar />
+      <div>hello world</div>
+      <br />
+      {!data ? (
+        <div>loading...</div>
+      ) : (
+        data.posts.map((post) => <div key={post.id}>{post.title}</div>)
+      )}
+    </>
+  );
+};
+
+export default withUrqlClient(createUrqlClient)(Index);
